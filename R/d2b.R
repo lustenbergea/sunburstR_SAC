f3e47469aab17b79fb29b9146535f46d54b2fad2
lustenbergea <109368459+lustenbergea@users.noreply.github.com>
2022-07-18 13:39:02 +0200

    Update d2b.R

--- R/d2b.R
+++ R/d2b.R
@@ -62,6 +62,21 @@ sund2b <- function(
   #  and convert to JSON with csv_to_hier
   #  this conversion should be more robust than
   #  the JavaScript converter
+    csv_to_hier <- function(csv, delim = "-", rootLabel = "root") {
+    hier_col <- strsplit(as.character(csv[[1]]), delim)
+    max_path_length <- max(unlist(lapply(hier_col, length)))
+    path_col_names <- paste0("X", seq_len(max_path_length))
+    df <- dplyr::bind_rows(lapply(hier_col, function(rw) {
+      structure(rw, names = path_col_names[1:(length(rw))])
+    }))
+    if (any(is.na(df[, 1]))) {
+      idx_notna <- which(!is.na(df[, 1]))
+      df[idx_notna, 2] <- df[idx_notna, 1]
+      df <- df[, -1]
+    }
+    df$size = csv[[2]]
+    d3r::d3_nest(df, value_cols = "size", root = rootLabel)
+  }
   if( inherits(data, "data.frame") )  {
     data = csv_to_hier(data)
   }
